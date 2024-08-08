f49dda8fcd6eedf4c4f59736d6d569c61b425b2c
aliseena1 <aliseena@live.unc.edu>
2024-08-08 15:59:52 -0400

    Added name and ski level form once signup occurs

--- client/src/components/pages/VerifyUser.jsx
+++ client/src/components/pages/VerifyUser.jsx
@@ -1,10 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAuthToken } from "../../AuthTokenContext";
 import { useNavigate } from "react-router-dom";
+import '../../style/Home.css';
 
 export default function VerifyUser() {
+  const [showNameForm, setShowNameForm] = useState(false); // If name form should be displayed
+  const [name, setName] = useState('');
+  const [skierType, setSkierType] = useState('');
+  const [skierLevel, setSkierLevel] = useState('');
   const navigate = useNavigate();
   const { accessToken } = useAuthToken();
+  const API_URL = process.env.REACT_APP_API_URL;
 
   useEffect(() => {
     async function verifyUser() {
@@ -19,8 +25,11 @@ export default function VerifyUser() {
       });
       const user = await data.json();
 
+      if (user.name === user.email) {
+        setShowNameForm(true);
+      }
       // TODO: redirect here to where the user should go after verifying their account
-      if (user.auth0Id) {
+      if (user.auth0Id && user.name !== user.email) {
         navigate("/");
       }
     }
@@ -30,9 +39,62 @@ export default function VerifyUser() {
     }
   }, [accessToken, navigate]);
 
+  // Update user profile information, then navigate to home page
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const response = await fetch(`${API_URL}/user`, {
+          method: 'PUT',
+          headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${accessToken}`
+          },
+          body: JSON.stringify({
+              name,
+              skierType,
+              skierLevel
+          })
+      });
+      if (!response.ok) {
+          throw new Error('Network response was not ok');
+      }
+      setShowNameForm(false);
+      navigate('/');
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <div className="loading">
-      <h1>Loading...</h1>
-    </div>
+    <>
+      {showNameForm ? (
+        <div className="name-form">
+          <p id="form-title">One last thing...</p>
+          <form className="form-content" onSubmit={handleSubmit}>
+            <label className="form-label" htmlFor="name">Your name:</label>
+              <input className="form-select" id="text-input" type="text" placeholder="Name" onChange={(e) => setName(e.target.value)} required></input>
+            <label className="form-label" htmlFor="skierType">Skier or Snowboarder?</label>
+              <select className="form-select" id="skierType" value={skierType} onChange={(e) => setSkierType(e.target.value)} required>
+                <option value="">--Please choose an option--</option>
+                <option value="Ski">Skier</option>
+                <option value="Snowboard">Snowboarder</option>
+              </select>
+            <label className="form-label" htmlFor="skierLevel">Skier level:</label>
+              <select className="form-select" id="skierLevel" value={skierLevel} onChange={(e) => setSkierLevel(e.target.value)} required>
+                <option value="">--Please choose an option--</option>
+                <option value="beginner">Beginner</option>
+                <option value="intermediate">Intermediate</option>
+                <option value="advanced">Advanced</option>
+              </select>
+            <button className="form-submit" type="submit">Submit</button>
+          </form>
+        </div>
+      ) : (
+        <div className="loading">
+          <h1>Loading...</h1>
+        </div>
+      )}
+    </>
   );
 }
